448985469c07e3d3bf69f371233fd34160982119
Kornel <kornel@geekhood.net>
2018-08-15 23:44:22 +0100

    Use connection per thread

--- src/lib_crate_db.rs
+++ src/lib_crate_db.rs
@@ -1,7 +1,8 @@
 extern crate rusqlite;
 extern crate chrono;
 extern crate categories;
-extern crate failure;
+#[macro_use] extern crate failure;
+extern crate thread_local;
 #[macro_use] extern crate lazy_static;
 extern crate rich_crate;
 use std::collections::HashMap;
@@ -11,10 +12,11 @@ use rich_crate::Repo;
 use rich_crate::Markup;
 use rich_crate::RichCrateVersion;
 use rusqlite::*;
+use thread_local::ThreadLocal;
 use chrono::prelude::*;
-use std::path::Path;
-use std::sync::Mutex;
+use std::path::PathBuf;
 use failure::ResultExt;
+use std::cell::RefCell;
 type Result<T> = std::result::Result<T, failure::Error>;
 
 mod schema;
@@ -22,24 +24,47 @@ mod stopwords;
 use stopwords::STOPWORDS;
 
 pub struct CrateDb {
-    pub(crate) conn: Mutex<Connection>,
+    path: PathBuf,
+    pub(crate) conn: ThreadLocal<std::result::Result<RefCell<Connection>, rusqlite::Error>>,
 }
 
 impl CrateDb {
     /// Path to sqlite db file to create/update
-    pub fn new(path: impl AsRef<Path>) -> Result<Self> {
-        let db = Self::db(path.as_ref()).context("schema creation failed")?;
-        db.execute("PRAGMA synchronous = 0", &[])?;
+    pub fn new(path: impl Into<PathBuf>) -> Result<Self> {
         Ok(Self {
-            conn: Mutex::new(db),
+            path: path.into(),
+            conn: ThreadLocal::new(),
         })
+    }
+
+    #[inline]
+    fn with_connection<F, T>(&self, cb: F) -> Result<T> where F: FnOnce(&mut Connection) -> Result<T> {
+        let conn = self.conn.get_or(|| Box::new(self.connect().map(RefCell::new)));
+        match conn {
+            Ok(conn) => cb(&mut *conn.borrow_mut()),
+            Err(err) => bail!("{}", err),
+        }
+    }
+
+    #[inline]
+    fn with_tx<F, T>(&self, cb: F) -> Result<T> where F: FnOnce(&Connection) -> Result<T> {
+        self.with_connection(|conn| {
+            let tx = conn.transaction()?;
+            let res = cb(&tx)?;
+            tx.commit()?;
+            Ok(res)
+        })
+    }
+
+    fn connect(&self) -> std::result::Result<Connection, rusqlite::Error> {
+        let db = Self::db(&self.path)?;
+        db.execute("PRAGMA synchronous = 0", &[])?;
+        Ok(db)
     }
 
     /// Add data of the latest version of a crate to the index
     pub fn index_latest(&self, c: &RichCrateVersion) -> Result<()> {
-        let mut conn = self.conn.lock().unwrap();
-        let tx = conn.transaction().context("index_latest tx")?;
-        {
+        self.with_tx(|tx| {
             let mut insert_crate = tx.prepare_cached("INSERT OR IGNORE INTO crates (origin, recent_downloads) VALUES (?1, ?2)")?;
             let mut insert_repo = tx.prepare_cached("INSERT OR REPLACE INTO crate_repos (crate_id, repo) VALUES (?1, ?2)")?;
             let mut delete_repo = tx.prepare_cached("DELETE FROM crate_repos WHERE crate_id = ?1")?;
@@ -126,10 +151,9 @@ impl CrateDb {
                     insert_keyword.add(&format!(":{}", feat), 0.55, false)?;
                 }
             }
-        }
-        tx.commit().context("commit crate upd")?;
-        println!("");
-        Ok(())
+            println!("");
+            Ok(())
+        })
     }
 
     /// Update crate <> repo association
@@ -147,42 +171,39 @@ impl CrateDb {
     ///    interesting relationship information for crates.
     pub fn index_repo_crates(&self, repo: &Repo, paths_and_names: impl Iterator<Item = (impl AsRef<str>, impl AsRef<str>)>) -> Result<()> {
         let repo = repo.canonical_git_url();
-        let mut conn = self.conn.lock().unwrap();
-        let tx = conn.transaction().context("index_repo_crates tx")?;
-        {
+        self.with_tx(|tx| {
             let mut insert_repo = tx.prepare_cached("INSERT OR IGNORE INTO repo_crates (repo, path, crate_name) VALUES (?1, ?2, ?3)")?;
             for (path, name) in paths_and_names {
                 let name = name.as_ref();
                 let path = path.as_ref();
                 insert_repo.execute(&[&repo.as_ref(), &path, &name]).context("repo rev insert")?;
             }
-        }
-        tx.commit().context("commit rev repo")?;
-        Ok(())
+            Ok(())
+        })
     }
 
     /// Returns crate name (not origin)
-    pub fn parent_crate(&self, repo: &Repo, child_name: &str) -> Option<String> {
-        let conn = self.conn.lock().unwrap();
-        let mut paths = conn.prepare_cached("SELECT path, crate_name FROM repo_crates WHERE repo = ?1 LIMIT 100").ok()?;
+    pub fn parent_crate(&self, repo: &Repo, child_name: &str) -> Result<Option<String>> {
+        self.with_connection(|conn| {
+        let mut paths = conn.prepare_cached("SELECT path, crate_name FROM repo_crates WHERE repo = ?1 LIMIT 100")?;
         let mut paths: HashMap<String, String> = paths
-            .query_map(&[&repo.canonical_git_url()], |r| (r.get(0), r.get(1)))
-            .ok()?
-            .collect::<std::result::Result<_, _>>().ok()?;
+            .query_map(&[&repo.canonical_git_url()], |r| (r.get(0), r.get(1)))?
+            .collect::<std::result::Result<_, _>>()?;
 
         if paths.len() < 2 {
-            return None;
+            return Ok(None);
         }
 
-        let child_path = paths.iter().find(|(_, child)| *child == child_name)
-            .map(|(path, _)| path.to_owned())?;
+        let child_path = if let Some(a) = paths.iter().find(|(_, child)| *child == child_name)
+            .map(|(path, _)| path.to_owned()) {a} else {return Ok(None)};
+
         paths.remove(&child_path);
         let mut child_path = child_path.as_str();
 
         loop {
              child_path = child_path.rsplitn(1, '/').nth(1).unwrap_or("");
             if let Some(child) = paths.get(child_path) {
-                return Some(child.to_owned());
+                return Ok(Some(child.to_owned()));
             }
             if child_path.is_empty() { // in these paths "" is the root
                 break;
@@ -202,20 +223,20 @@ impl CrateDb {
             s
         }
 
-        if let Some(child) = repo.repo_name().and_then(|n| paths.get(n).or_else(|| paths.get(unprefix(n)))) {
-            return Some(child.to_owned());
+        Ok(if let Some(child) = repo.repo_name().and_then(|n| paths.get(n).or_else(|| paths.get(unprefix(n)))) {
+            Some(child.to_owned())
         }
-        if let Some(child) = repo.owner_name().and_then(|n| paths.get(n).or_else(|| paths.get(unprefix(n)))) {
-            return Some(child.to_owned());
-        }
-        None
+        else if let Some(child) = repo.owner_name().and_then(|n| paths.get(n).or_else(|| paths.get(unprefix(n)))) {
+            Some(child.to_owned())
+        } else {
+            None
+        })
+        })
     }
 
     pub fn index_repo_changes(&self, repo: &Repo, changes: &[RepoChange]) -> Result<()> {
         let repo = repo.canonical_git_url();
-        let mut conn = self.conn.lock().unwrap();
-        let tx = conn.transaction().context("index_repo_changes tx")?;
-        {
+        self.with_tx(|tx| {
             let mut insert_change = tx.prepare_cached("INSERT OR IGNORE INTO repo_changes (repo, crate_name, replacement, weight) VALUES (?1, ?2, ?3, ?4)")?;
             for change in changes {
                 match *change {
@@ -227,23 +248,21 @@ impl CrateDb {
                     },
                 }?;
             }
-        }
-        tx.commit().context("index_repo_changes c")?;
-        Ok(())
+            Ok(())
+        })
     }
 
     pub fn path_in_repo(&self, repo: &Repo, crate_name: &str) -> Result<String> {
         let repo = repo.canonical_git_url();
-        let conn = self.conn.lock().unwrap();
+        self.with_connection(|conn| {
         let mut get_path = conn.prepare_cached("SELECT path FROM repo_crates WHERE repo = ?1 AND crate_name = ?2")?;
         Ok(get_path.query_row(&[&repo, &crate_name], |row| row.get(0)).context("path_in_repo")?)
+        })
     }
 
     /// Update download counts of the crate
     pub fn index_versions(&self, all: &RichCrate) -> Result<()> {
-        let mut conn = self.conn.lock().unwrap();
-        let tx = conn.transaction().context("index_versions tx")?;
-        {
+        self.with_tx(|tx| {
             let mut update_recent = tx.prepare_cached("UPDATE crates SET recent_downloads = ?1 WHERE id = ?2")?;
             let mut get_crate_id = tx.prepare_cached("SELECT id FROM crates WHERE origin = ?1")?;
             let mut insert_version = tx.prepare_cached("INSERT OR IGNORE INTO crate_versions (crate_id, version, created) VALUES (?1, ?2, ?3)")?;
@@ -266,16 +285,15 @@ impl CrateDb {
                 let downloads = dl.downloads as u32;
                 insert_dl.execute(&[&crate_id, &period, &ver, &downloads]).context("insert dl")?;
             }
-        }
-        tx.commit().context("versions commit")?;
-        Ok(())
+            Ok(())
+        })
     }
 
     /// Guess categories for a crate
     ///
     /// Returns category slugs
     pub fn categories(&self, origin: &Origin) -> Result<Vec<String>> {
-        let conn = self.conn.lock().unwrap();
+        self.with_connection(|conn| {
         let mut query = conn.prepare_cached(r#"
         select sum(cc.weight * ck.weight * relk.relevance)/(8+count(*)) as w, cc.slug
         from (
@@ -302,14 +320,15 @@ impl CrateDb {
         limit 1"#).context("categories")?;
         let all = query.query_map(&[&origin.to_str()], |row| row.get(1)).context("categories q")?;
         Ok(all.collect::<std::result::Result<_,_>>()?)
+        })
     }
 
     /// Find most relevant category for a crate, and how popular the crate is
     ///
     /// Returns (top n-th, category slug)
-    pub fn top_category(&self, origin: &Origin) -> Option<(u32, String)> {
-        let conn = self.conn.lock().unwrap();
-        let mut query = match conn.prepare_cached(r#"
+    pub fn top_category(&self, origin: &Origin) -> Result<(u32, String)> {
+        self.with_connection(|conn| {
+        let mut query = conn.prepare_cached(r#"
             select count(*) as top, cc.slug from crates c
             join categories cc on cc.crate_id = c.id
             join categories occ on occ.slug = cc.slug
@@ -319,19 +338,17 @@ impl CrateDb {
             group by cc.slug
             order by 1
             limit 1
-        "#) {
-            Ok(o) => o,
-            Err(_) => return None,
-        };
-        query.query_row(&[&origin.to_str()], |row| (row.get(0), row.get(1))).ok()
+        "#)?;
+        Ok(query.query_row(&[&origin.to_str()], |row| (row.get(0), row.get(1)))?)
+        })
     }
 
     /// Find most relevant keyword for the crate
     ///
     /// Returns (top n-th for the keyword, the keyword)
-    pub fn top_keyword(&self, origin: &Origin) -> Option<(u32, String)> {
-        let conn = self.conn.lock().unwrap();
-        let mut query = match conn.prepare_cached(r#"
+    pub fn top_keyword(&self, origin: &Origin) -> Result<(u32, String)> {
+        self.with_connection(|conn| {
+        let mut query = conn.prepare_cached(r#"
             select top, keyword from (
                 select count(*) as total, sum(case when oc.recent_downloads >= c.recent_downloads then 1 else 0 end) as top, k.keyword from crates c
                         join crate_keywords ck on ck.crate_id = c.id
@@ -346,16 +363,14 @@ impl CrateDb {
                         having count(*) >= 5
                 ) as tmp
             order by top + (top+30.0)/total
-        "#) {
-            Ok(o) => o,
-            Err(_) => return None,
-        };
-        query.query_row(&[&origin.to_str()], |row| (row.get(0), row.get(1))).ok()
+        "#)?;
+        Ok(query.query_row(&[&origin.to_str()], |row| (row.get(0), row.get(1)))?)
+        })
     }
 
     /// Categories similar to the given category
     pub fn related_categories(&self, slug: &str) -> Result<Vec<String>> {
-        let conn = self.conn.lock().unwrap();
+        self.with_connection(|conn| {
         let mut query = conn.prepare_cached(r#"
             select sum(c2.weight * c1.weight) as w, c2.slug
             from categories c1
@@ -369,10 +384,11 @@ impl CrateDb {
         "#)?;
         let res = query.query_map(&[&slug], |row| row.get(1)).context("related_categories")?;
         Ok(res.collect::<std::result::Result<_,_>>()?)
+        })
     }
 
     pub fn replacement_crates(&self, crate_name: &str) -> Result<Vec<String>> {
-        let conn = self.conn.lock().unwrap();
+        self.with_connection(|conn| {
         let mut query = conn.prepare_cached(r#"
             SELECT sum(weight) as w, replacement
             FROM repo_changes
@@ -385,10 +401,11 @@ impl CrateDb {
         "#)?;
         let res = query.query_map(&[&crate_name], |row| row.get(1)).context("replacement_crates")?;
         Ok(res.collect::<std::result::Result<_,_>>()?)
+        })
     }
 
     pub fn related_crates(&self, origin: &Origin) -> Result<Vec<Origin>> {
-        let conn = self.conn.lock().unwrap();
+        self.with_connection(|conn| {
         let mut query = conn.prepare_cached(r#"
             SELECT sum(k2.weight * k1.weight) as w, c2.origin
             FROM crates c1
@@ -408,11 +425,12 @@ impl CrateDb {
             Origin::from_string(s)
         }).context("related_crates")?;
         Ok(res.collect::<std::result::Result<_,_>>()?)
+        })
     }
 
     /// Find keywords that may be most relevant to the crate
     pub fn keywords(&self, origin: &Origin) -> Result<Vec<String>> {
-        let conn = self.conn.lock().unwrap();
+        self.with_connection(|conn| {
         let mut query = conn.prepare_cached(r#"
             select avg(ck.weight) * srck.weight, k.keyword
             -- find the crate to categorize
@@ -442,11 +460,12 @@ impl CrateDb {
                 None
             }
         }).collect())
+        })
     }
 
     /// Find most relevant/popular keywords in the category
     pub fn top_keywords_in_category(&self, slug: &str) -> Result<Vec<String>> {
-        let conn = self.conn.lock().unwrap();
+        self.with_connection(|conn| {
         let mut query = conn.prepare_cached(r#"
             select sum(k.weight), kk.keyword from categories c
                 join crate_keywords k using(crate_id)
@@ -460,11 +479,12 @@ impl CrateDb {
         let q = query.query_map(&[&slug], |row| row.get(1)).context("top keywords")?;
         let q = q.filter_map(|r| r.ok());
         Ok(q.collect())
+        })
     }
 
     /// Most popular crates in the category
     pub fn top_crates_in_category(&self, slug: &str, limit: u32, simple_sort: bool) -> Result<Vec<(Origin, u32)>> {
-        let conn = self.conn.lock().unwrap();
+        self.with_connection(|conn| {
 
         let mut query = conn.prepare_cached(if simple_sort {
             // sort by popularity
@@ -490,13 +510,14 @@ impl CrateDb {
         })?;
         let q = q.filter_map(|r| r.ok());
         Ok(q.collect())
+        })
     }
 
     /// Newly added or updated crates in the category
     ///
     /// Returns `origin` strings
     pub fn recently_updated_crates_in_category(&self, slug: &str) -> Result<Vec<Origin>> {
-        let conn = self.conn.lock().unwrap();
+        self.with_connection(|conn| {
         let mut query = conn.prepare_cached(r#"
             select max(created), k.origin
                 from categories c
@@ -513,11 +534,12 @@ impl CrateDb {
         })?;
         let q = q.filter_map(|r| r.ok());
         Ok(q.collect())
+        })
     }
 
     /// Number of crates in every category
     pub fn category_crate_counts(&self) -> Result<HashMap<String, u32>> {
-        let conn = self.conn.lock().unwrap();
+        self.with_connection(|conn| {
         let mut q = conn.prepare(r#"
             select c.slug, count(*) as cnt from categories c group by c.slug
         "#)?;
@@ -525,6 +547,7 @@ impl CrateDb {
             (row.get(0), row.get(1))
         }).context("counts")?.filter_map(|r| r.ok());
         Ok(q.collect())
+        })
     }
 
     fn extract_text(c: &RichCrateVersion) -> Option<(f64,&str)> {

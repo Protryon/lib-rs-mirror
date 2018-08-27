c8af84decc7428b98f6c7d9b2b3435b9ba0983cb
Kornel <kornel@geekhood.net>
2018-08-27 12:23:41 +0100

    Bump

--- src/bin/reindex_users.rs
+++ src/bin/reindex_users.rs
@@ -8,6 +8,7 @@ use repo_url::Repo;
 use repo_url::RepoHost;
 use std::sync::Arc;
 use std::sync::mpsc;
+use std::sync::Mutex;
 use std::thread;
 use std::collections::HashSet;
 use kitchen_sink::{KitchenSink, CrateData, Origin};
@@ -22,27 +23,19 @@ fn main() {
     });
     let crates2 = crates.clone();
     let (tx, rx) = mpsc::sync_channel(64);
-    let mut seen = HashSet::new();
+    let seen_repo = Arc::new(Mutex::new(HashSet::new()));
 
     thread::spawn(move || {
         let tx1 = tx.clone();
         rayon::scope(move |s1| {
             for k in crates.all_crates() {
                 let crates = Arc::clone(&crates);
+                let seen_repo = Arc::clone(&seen_repo);
                 let tx = tx1.clone();
                 s1.spawn(move |_| {
                     println!("{:?}", k.name());
                     let r1 = crates.rich_crate_version(&Origin::from_crates_io_name(k.name()), CrateData::Minimal);
                     let res = r1.and_then(|c| {
-                        if let Some(Repo{host:RepoHost::GitHub(repo),..}) = c.repository() {
-                            if let Ok(commits) = crates.repo_commits(&repo) {
-                                for c in commits {
-                                    crates.index_user(&c.author, &c.commit.author)?;
-                                    crates.index_user(&c.committer, &c.commit.committer)?;
-                                }
-                            }
-                        }
-
                         for a in c.authors().iter().filter(|a| a.email.is_some()) {
                             if let Some(email) = a.email.as_ref() {
                                 tx.send(Some((email.to_string(), a.name.clone())))?;
@@ -62,6 +55,7 @@ fn main() {
         tx.send(None).unwrap();
     });
 
+    let mut seen = HashSet::new();
     while let Some((email, name)) = rx.recv().unwrap() {
         let email: String = email;
         let name: Option<String> = name;

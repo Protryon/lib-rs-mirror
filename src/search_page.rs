d8aee7bff1097b729a3b74bb3c460c74e8b6ef52
Kornel <kornel@geekhood.net>
2018-12-27 16:30:12 +0000

    More data on SERP

--- src/search_page.rs
+++ src/search_page.rs
@@ -13,7 +13,7 @@ pub struct SearchPage<'a> {
 
 impl SearchPage<'_> {
     pub fn new<'a>(query: &'a str, results: &'a [search_index::CrateFound], markup: &'a Renderer) -> SearchPage<'a> {
-        let half_score = results.get(0).map_or(0., |r| r.score) * 0.4;
+        let half_score = results.get(0).map_or(0., |r| r.score) * 0.33;
         let num = results.iter().take_while(|r| r.score >= half_score).count();
         let (good_results, bad_results) = results.split_at(num);
         SearchPage {
@@ -42,6 +42,37 @@ impl SearchPage<'_> {
             canonical: None,
             noindex: true,
             critical_css_data: Some(include_str!("../../style/public/search.css")),
+        }
+    }
+
+    /// For color of the version
+    ///
+    /// It tries to guess which versions seem "unstable".
+    ///
+    /// TODO: Merge with the better version history analysis from the individual crate page.
+    pub fn version_class(&self, ver: &str) -> &str {
+        let v = semver::Version::parse(ver).expect("semver");
+        match (v.major, v.minor, v.patch, v.is_prerelease()) {
+            (1..=15, _, _, false) => "stable",
+            (0, m, p, false) if m >= 2 && p >= 3 => "stable",
+            (m, ..) if m >= 1 => "okay",
+            (0, 1, p, _) if p >= 10 => "okay",
+            (0, 3..=10, p, _) if p > 0 => "okay",
+            _ => "unstable",
+        }
+    }
+
+    /// Nicely rounded number of downloads
+    ///
+    /// To show that these numbers are just approximate.
+    pub fn downloads(&self, num: u64) -> (String, &str) {
+        match num {
+            a @ 0..=99 => (format!("{}", a), ""),
+            a @ 0..=500 => (format!("{}", a / 10 * 10), ""),
+            a @ 0..=999 => (format!("{}", a / 50 * 50), ""),
+            a @ 0..=9999 => (format!("{}.{}", a / 1000, a % 1000 / 100), "K"),
+            a @ 0..=999_999 => (format!("{}", a / 1000), "K"),
+            a => (format!("{}.{}", a / 1_000_000, a % 1_000_000 / 100_000), "M"),
         }
     }
 

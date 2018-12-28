a557c9483c150b7225529ad13674d6a2e2abb795
Kornel <kornel@geekhood.net>
2018-12-28 02:10:19 +0000

    Script fix

--- reindex.sh
+++ reindex.sh
@@ -2,4 +2,5 @@
 source ../front_end/conf.sh;
 cargo build --release --bin reindex_crates
 cargo run --release --bin reindex_crates &
-( cd ../front_end; cargo build --release --bin website )
+( cd ../front_end; nice cargo build --release --bin website )
+wait

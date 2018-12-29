4feb16beb79ce98ca63727fc66fe9e8d4c6ab940
Kornel <kornel@geekhood.net>
2018-12-29 10:12:55 +0000

    Render real homepage and crate pages

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,44 @@
 use std::path::PathBuf;
-use std::borrow::Cow;
-use actix_web::{http::*, *};
+use actix_web::*;
+use actix_web::http::*;
 use env_logger;
 use futures::future::{self, Future};
 use futures_cpupool::CpuPool;
 use std::sync::Arc;
 use std::env;
+use kitchen_sink;
+use kitchen_sink::KitchenSink;
+use kitchen_sink::Origin;
+use kitchen_sink::CrateData;
 use front_end;
 use search_index::CrateSearchIndex;
-use render_readme::{Highlighter, ImageFilter, Renderer};
-
-struct NopFilter {}
-impl ImageFilter for NopFilter {
-    fn filter_url<'a>(&self, _: &'a str) -> Cow<'a, str> {
-        "#nope".into()
-    }
-
-    fn image_size(&self, _: &str) -> Option<(u32, u32)> {
-        None
-    }
-}
+use render_readme::{Highlighter, Renderer, ImageOptimAPIFilter};
 
 struct ServerState {
     pool: CpuPool,
     markup: Renderer,
     index: CrateSearchIndex,
+    crates: KitchenSink,
 }
 
 type AServerState = Arc<ServerState>;
 
 fn main() {
     env_logger::init();
+    kitchen_sink::dont_hijack_ctrlc();
     let sys = actix::System::new("crates-server");
 
-    let markup = Renderer::new_filter(Highlighter::new(), Arc::new(NopFilter {}));
 
     let public_dir: PathBuf = env::var_os("DOCUMENT_ROOT").map(From::from).unwrap_or_else(|| "../style/public".into());
     let data_dir: PathBuf = env::var_os("CRATE_DATA_DIR").map(From::from).unwrap_or_else(|| "../data".into());
+    let github_token = env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN missing");
 
     assert!(public_dir.exists(), "DOCUMENT_ROOT {} does not exist", public_dir.display());
     assert!(data_dir.exists(), "CRATE_DATA_DIR {} does not exist", data_dir.display());
+
+    let crates = KitchenSink::new(&data_dir, &github_token).unwrap();
+    let image_filter = Arc::new(ImageOptimAPIFilter::new("czjpqfbdkz", crates.main_cache_dir().join("images.db")).unwrap());
+    let markup = Renderer::new_filter(Highlighter::new(), image_filter);
 
     let index = CrateSearchIndex::new(data_dir).expect("data directory");
 
@@ -47,13 +46,21 @@ fn main() {
         pool: CpuPool::new_num_cpus(),
         markup,
         index,
+        crates,
+    });
+
+    let state2 = Arc::clone(&state);
+    let _ = std::thread::spawn(move || {
+        state2.crates.prewarm();
     });
 
     server::new(move || {
         App::with_state(state.clone())
             .middleware(middleware::Logger::default())
+            .resource("/", |r| r.method(Method::GET).f(handle_home))
             .resource("/search", |r| r.method(Method::GET).f(handle_search))
             .resource("/keywords/{keyword}", |r| r.method(Method::GET).f(handle_keyword))
+            .resource("/crates/{crate}", |r| r.method(Method::GET).f(handle_crate))
             .handler("/", fs::StaticFiles::new(&public_dir).expect("public directory"))
             .default_resource(|r| r.f(handle_404))
     })
@@ -62,12 +69,54 @@ fn main() {
     .shutdown_timeout(0) // <- Set shutdown timeout to 0 seconds (default 60s)
     .start();
 
-    println!("Starting HTTP server on 127.0.0.1:32531");
+    println!("Starting HTTP server on http://127.0.0.1:32531");
     let _ = sys.run();
 }
 
 fn handle_404(_req: &HttpRequest<AServerState>) -> Result<HttpResponse> {
     Ok(HttpResponse::NotFound().content_type("text/plain;charset=UTF-8").body("404\n"))
+}
+
+fn handle_home(req: &HttpRequest<AServerState>) -> FutureResponse<HttpResponse> {
+    let state = req.state();
+    let state2 = Arc::clone(state);
+    state.pool.spawn_fn(move || {
+        let mut page: Vec<u8> = Vec::with_capacity(50000);
+        front_end::render_homepage(&mut page, &state2.crates).unwrap();
+        Ok::<_,()>(page)
+    })
+    .map_err(|_| unreachable!())
+    .and_then(|page| {
+        future::ok(HttpResponse::Ok()
+            .content_type("text/html;charset=UTF-8")
+            .content_length(page.len() as u64)
+            .body(page))
+    })
+    .responder()
+}
+
+fn handle_crate(req: &HttpRequest<AServerState>) -> FutureResponse<HttpResponse> {
+    let kw: String = req.match_info().query("crate").unwrap();
+    println!("rendering {:?}", kw);
+    let origin = Origin::from_crates_io_name(&kw);
+    let state = req.state();
+    let state2 = Arc::clone(state);
+    state.pool.spawn_fn(move || {
+        let all = state2.crates.rich_crate(&origin).unwrap();
+        let ver = state2.crates.rich_crate_version(&origin, CrateData::Full).unwrap();
+        let mut page: Vec<u8> = Vec::with_capacity(50000);
+        front_end::render_crate_page(&mut page, &all, &ver, &state2.crates, &state2.markup).unwrap();
+        Ok::<_,()>(page)
+    })
+    .map_err(|_| unreachable!())
+    .and_then(|page| {
+        future::ok(HttpResponse::Ok()
+            .content_type("text/html;charset=UTF-8")
+            .header("Cache-Control", "max-age=172800, stale-while-revalidate=604800")
+            .content_length(page.len() as u64)
+            .body(page))
+    })
+    .responder()
 }
 
 fn handle_keyword(req: &HttpRequest<AServerState>) -> FutureResponse<HttpResponse> {
@@ -96,6 +145,7 @@ fn handle_keyword(req: &HttpRequest<AServerState>) -> FutureResponse<HttpRespons
                 future::ok(if let Some(page) = page {
                     HttpResponse::Ok()
                         .content_type("text/html;charset=UTF-8")
+                        .header("Cache-Control", "max-age=604800, stale-while-revalidate=604800, stale-if-error=86400")
                         .content_length(page.len() as u64)
                         .body(page)
                 } else {
@@ -110,6 +160,7 @@ fn handle_keyword(req: &HttpRequest<AServerState>) -> FutureResponse<HttpRespons
         _ => {
             future::ok(HttpResponse::TemporaryRedirect()
                 .header("Location", "/")
+                .header("Cache-Control", "max-age=172800, stale-while-revalidate=604800, stale-if-error=86400")
                 .finish())
                 .responder()
         },

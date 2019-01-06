f2e60da67641bcbed52549d72ac729a49765c3ce
Kornel <kornel@geekhood.net>
2019-01-06 21:02:38 +0000

    Pass arg

--- reindex.sh
+++ reindex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ../front_end/conf.sh;
 cargo build --release --bin reindex_crates
-cargo run --release --bin reindex_crates &
+cargo run --release --bin reindex_crates -- "$@" &
 ( cd ../front_end; nice cargo build --release --bin website )
 wait

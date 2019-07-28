f39bc3edd19f894d956f950226bde54761e09fbb
Kornel <kornel@geekhood.net>
2019-07-28 12:49:45 +0100

    Support mac-published readmes

--- src/crate_git_checkout.rs
+++ src/crate_git_checkout.rs
@@ -302,7 +302,7 @@ fn readme_from_repo(markup: Markup, repo_url: &Option<String>, base_dir_in_repo:
 fn is_readme_filename(path: &Path, package: Option<&Package>) -> bool {
     path.to_str().map_or(false, |s| {
         if let Some(&Package{readme: Some(ref r),..}) = package {
-            r == s
+            r.eq_ignore_ascii_case(s) // crates published on Mac have this
         } else {
             render_readme::is_readme_filename(path)
         }

41137669699f308ad064d0bd3151a4fb58e89c47
Kornel <kornel@geekhood.net>
2019-07-31 14:26:53 +0100

    Case-fold keywords

--- front_end/src/search_page.rs
+++ front_end/src/search_page.rs
@@ -46,8 +46,8 @@ impl SearchPage<'_> {
             SearchKind::Query(s) | SearchKind::Keyword(s) => s,
         };
         for res in self.good_results.iter().chain(self.bad_results.iter()) {
-            for keyword in res.keywords.split(", ").filter(|&k| !k.is_empty() && k != query) {
-                let cnt = counts.entry(keyword).or_insert((0u32,0f32));
+            for keyword in res.keywords.split(", ").filter(|&k| !k.is_empty() && !unicase::eq_ascii(k, query)) {
+                let cnt = counts.entry(unicase::Ascii::new(keyword)).or_insert((0u32,0f32));
                 cnt.0 += 1;
                 cnt.1 += res.score;
             }
@@ -56,7 +56,7 @@ impl SearchPage<'_> {
             // keep if more than 1 crate has it
             // but don't repeat terms from the query
             .filter(|(_, (n, _))| *n > 1 && *n < obvious_threshold)
-            .map(|(k, (_, v))| (k,v)).collect();
+            .map(|(k, (_, v))| (k.into_inner(),v)).collect();
         counts.sort_by(|a,b| b.1.partial_cmp(&a.1).unwrap_or(std::cmp::Ordering::Equal));
         counts.into_iter().take(6).map(|(k,_)| k).collect()
     }

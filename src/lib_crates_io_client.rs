f393c4ec32e2ff0c7989d57345e31bb3a6ff592e
Kornel <kornel@geekhood.net>
2019-12-26 18:04:35 +0100

    Delete downloads functionality

--- src/lib_crates_io_client.rs
+++ src/lib_crates_io_client.rs
@@ -1,20 +1,16 @@
 pub use simple_cache::Error;
-use chrono::{Date, TimeZone, Utc};
 use mysteriouspants_throttle::Throttle;
 use parking_lot::Mutex;
 use serde_derive::*;
 use simple_cache::SimpleCache;
 use simple_cache::TempCache;
-use std::collections::HashMap;
-use std::collections::HashSet;
+
 use std::path::Path;
 use urlencoding::encode;
 mod crate_deps;
-mod crate_downloads;
 mod crate_meta;
 mod crate_owners;
 pub use crate::crate_deps::*;
-pub use crate::crate_downloads::*;
 pub use crate::crate_meta::*;
 pub use crate::crate_owners::*;
 
@@ -36,7 +32,6 @@ macro_rules! cioopt {
 #[derive(Debug)]
 pub struct CratesIoCrate {
     pub meta: CrateMetaFile,
-    pub downloads: CrateDownloadsFile,
     pub owners: Vec<CrateOwner>,
 }
 
@@ -73,52 +68,21 @@ impl CratesIoClient {
         self.crates.get_cached((&key, version), &url)
     }
 
-    pub fn krate(&self, crate_name: &str, cache_buster: &str, refresh: bool) -> Result<Option<CratesIoCrate>, Error> {
-        let (meta, (downloads, owners)) = rayon::join(
+    pub fn krate(&self, crate_name: &str, cache_buster: &str) -> Result<Option<CratesIoCrate>, Error> {
+        let (meta, owners) = rayon::join(
                 || self.crate_meta(crate_name, cache_buster),
-                || rayon::join(
-                    || self.crate_downloads(crate_name, cache_buster, refresh),
-                    || self.crate_owners(crate_name, cache_buster)));
-        let mut meta = cioopt!(meta?);
-        let downloads = cioopt!(downloads?);
+                || self.crate_owners(crate_name, cache_buster));
+        let meta = cioopt!(meta?);
         let owners = cioopt!(owners?);
 
-        if !self.cache.cache_only && !Self::are_downloads_consistent(&meta, &downloads) {
-            eprintln!("Meta is missing versions {}@{}", crate_name, cache_buster);
-            let _ = self.cache.delete(crate_name);
-            meta = cioopt!(self.crate_meta(crate_name, cache_buster)?);
-            assert!(Self::are_downloads_consistent(&meta, &downloads));
-        }
         Ok(Some(CratesIoCrate {
             meta,
-            downloads,
             owners,
         }))
     }
 
-    fn are_downloads_consistent(meta: &CrateMetaFile, downloads: &CrateDownloadsFile) -> bool {
-        let versions: HashSet<_> = meta.versions.iter().map(|v| v.id).collect();
-        downloads.version_downloads.iter().all(|d| {
-            versions.contains(&d.version)
-        })
-    }
-
     pub fn crate_meta(&self, crate_name: &str, as_of_version: &str) -> Result<Option<CrateMetaFile>, Error> {
         self.get_json((crate_name, as_of_version), encode(crate_name))
-    }
-
-    pub fn crate_downloads(&self, crate_name: &str, as_of_version: &str, refresh: bool) -> Result<Option<CrateDownloadsFile>, Error> {
-        let url = format!("{}/downloads", encode(crate_name));
-        let new_key = (url.as_str(), as_of_version);
-        let data: CrateDownloadsFile = cioopt!(self.get_json(new_key, &url)?);
-        if refresh && !self.cache.cache_only && data.is_stale() {
-            eprintln!("downloads expired {}@{}", crate_name, as_of_version);
-            let _ = self.cache.delete(new_key.0);
-            let fresh: CrateDownloadsFile = cioopt!(self.get_json(new_key, &url)?);
-            Ok(Some(fresh))
-        } else {
-            Ok(Some(data))
-        }
     }
 
     pub fn crate_owners(&self, crate_name: &str, as_of_version: &str) -> Result<Option<Vec<CrateOwner>>, Error> {
@@ -169,7 +133,6 @@ enum Payload {
     CrateMetaFile(CrateMetaFile),
     CrateOwnersFile(CrateOwnersFile),
     CrateTeamsFile(CrateTeamsFile),
-    CrateDownloadsFile(CrateDownloadsFile),
 }
 
 pub(crate) trait Payloadable {
@@ -187,51 +150,9 @@ impl Payloadable for CrateOwnersFile {
     fn from(val: Payload) -> Self { match val { Payload::CrateOwnersFile(d) => d, _ => panic!("bad cache") } }
 }
 
-impl Payloadable for CrateDownloadsFile {
-    fn to(&self) -> Payload { Payload::CrateDownloadsFile(self.clone()) }
-    fn from(val: Payload) -> Self { match val { Payload::CrateDownloadsFile(d) => d, _ => panic!("bad cache") } }
-}
-
 impl Payloadable for CrateTeamsFile {
     fn to(&self) -> Payload { Payload::CrateTeamsFile(self.clone()) }
     fn from(val: Payload) -> Self { match val { Payload::CrateTeamsFile(d) => d, _ => panic!("bad cache") } }
-}
-
-#[derive(Debug, Clone)]
-pub struct DailyVersionDownload<'a> {
-    pub version: Option<&'a CrateMetaVersion>,
-    pub downloads: usize,
-    pub date: Date<Utc>,
-}
-
-impl CratesIoCrate {
-    pub fn daily_downloads(&self) -> Vec<DailyVersionDownload<'_>> {
-        let versions: HashMap<_,_> = self.meta.versions.iter().map(|v| (v.id, v)).collect();
-        self.downloads.version_downloads.iter().map(|d| {
-            DailyVersionDownload {
-                version: versions.get(&d.version).cloned(),
-                downloads: d.downloads,
-                date: parse_date(&d.date),
-            }
-        })
-        .chain(
-            self.downloads.meta.extra_downloads.iter().map(|d| {
-                DailyVersionDownload {
-                    version: None,
-                    downloads: d.downloads,
-                    date: parse_date(&d.date),
-                }
-            })
-        )
-        .collect()
-    }
-}
-
-pub(crate) fn parse_date(date: &str) -> Date<Utc> {
-    let y = date[0..4].parse().expect("dl date parse");
-    let m = date[5..7].parse().expect("dl date parse");
-    let d = date[8..10].parse().expect("dl date parse");
-    Utc.ymd(y, m, d)
 }
 
 #[test]

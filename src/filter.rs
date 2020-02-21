0d19c73dd76b3aae0556d7764bf8ac8a7ae0a124
Kornel <kornel@geekhood.net>
2020-02-21 02:13:37 +0000

    Async

--- src/filter.rs
+++ src/filter.rs
@@ -37,15 +37,17 @@ pub struct ImageOptimAPIFilter {
     img2x_prefix: String,
     meta_prefix: String,
     cache: TempCache<ImageOptimImageMeta>,
+    handle: tokio::runtime::Handle,
 }
 
 impl ImageOptimAPIFilter {
-    pub fn new(api_id: &str, cache_path: impl Into<PathBuf>) -> Result<Self, Error> {
+    pub async fn new(api_id: &str, cache_path: impl Into<PathBuf>) -> Result<Self, Error> {
         Ok(Self {
             img_prefix: format!("https://img.gs/{}/full/", api_id),
             img2x_prefix: format!("https://img.gs/{}/full,2x/", api_id),
             meta_prefix: format!("https://img.gs/{}/meta,timeout=90/", api_id),
             cache: TempCache::new(cache_path)?,
+            handle: tokio::runtime::Handle::current(),
         })
     }
 }
@@ -67,8 +69,7 @@ impl ImageFilter for ImageOptimAPIFilter {
     fn image_size(&self, image_url: &str) -> Option<(u32, u32)> {
         let image_url = image_url.trim_start_matches(&self.img_prefix).trim_start_matches(&self.img2x_prefix);
         let api_url = format!("{}{}", self.meta_prefix, image_url);
-        self.cache
-            .get_json(image_url, api_url, |f| f)
+        self.handle.enter(|| futures::executor::block_on(self.cache.get_json(image_url, api_url, |f| f)))
             .map_err(|e| {
                 eprintln!("warning: image req to meta of {} failed: {}", image_url, e);
             })

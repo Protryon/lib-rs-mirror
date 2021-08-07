5e3fa265c03998a89677e24024fd8d5f2100823a
Kornel <kornel@geekhood.net>
2021-08-08 00:51:07 +0100

    builder

--- builder/src/main.rs
+++ builder/src/main.rs
@@ -33,20 +33,19 @@ RUN rustup toolchain add 1.45.0
 RUN rustup toolchain add 1.40.0
 RUN rustup toolchain add 1.35.0
 RUN rustup toolchain add 1.52.0
-RUN rustup toolchain add 1.44.0
+RUN rustup toolchain add 1.39.0
 RUN rustup toolchain list
 # RUN cargo new lts-dummy; cd lts-dummy; cargo lts setup; echo 'itoa = "*"' >> Cargo.toml; cargo update;
 "##;
 
 const TEMP_JUNK_DIR: &str = "/var/tmp/crates_env";
 
-const RUST_VERSIONS: [&str; 6] = [
+const RUST_VERSIONS: [&str; 5] = [
     "1.28.0",
-    "1.29.0",
-    "1.33.0",
-    "1.40.0",
-    "1.44.0",
+    "1.39.0",
+    "1.46.0",
     "1.52.0",
+    "1.53.0",
 ];
 
 use crate_db::builddb::*;

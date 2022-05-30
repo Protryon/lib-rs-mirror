9d5a03daacd0f9f6dd6ece0561258d8db21e1069
Kornel <kornel@geekhood.net>
2022-05-30 03:13:19 +0100

    Obsolete

--- reindex/reindex.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-source ../front_end/conf.sh;
-cargo build --release --bin reindex_crates
-cargo run --release --bin reindex_crates -- "$@" &
-( cd ../front_end; nice cargo build --release --bin website )
-wait
